0cc9c99a78d41245fae0c6807e99371831908e20
11440e <adm.11440e@asio.gov.au>
2024-01-22 16:14:24 +1100

    Updates to config

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,11 +1,29 @@
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "JMarkin/nvim-tree.lua-float-preview",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      view = {
+        float = {
+          enable = true,
+          quit_on_focus_loss = true,
+          open_win_config = {
+            relative = "editor",
+            border = "rounded",
+            width = 30,
+            height = 30,
+            row = 1,
+            col = 1,
+          },
+        },
+      }
+    }
   end,
+
 }
